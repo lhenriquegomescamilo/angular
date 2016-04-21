5c134003a74b4116be549e0b00fad80c82f21ee1
lcamilo <lcamilo@esec.com.br>
2016-04-21 01:31:51 -0300

    master: customizando libs

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+var express = require("express");
+
+var app = express();
+
+app.use(express.static(__dirname+"/public"));
+app.use(express.bodyParser());
+
+var contacts = [
+    {name: "Luis Henrique", date: new Date(), phone:"(61) 9409-0599", carrier : {name : "Oi", code: 14, category: "Cell"}},
+    {name: "Silvio Santos", date: new Date(), phone:"(62) 4609-0599", carrier : {name:"Tim", code: 41, category:"Cell"}},
+    {name: "Goms Camilo", date: new Date(), phone:"(61) 4409-0599", carrier : {name:"Claro", code: 15, category:"Cell"}},
+    {name: "Ana", date: new Date(), phone:"(62) 42342-0599", carrier : {name:"Brasil Telecon", code: 21, category:"Cell"}}
+];
+
+var  operators = [
+  {name:"Oi",code:14,category:"Cell"},
+  {name:"Tim",code:15,category:"Cell"},
+  {name:"Vivo",code:16,category:"Cell"},
+  {name:"Claro",code:17,category:"Cell"},
+  {name:"GVT",code:19,category:"Fixo"},
+  {name:"NET",code:20,category:"Fixo"},
+  {name:"Embratel",code:21,category:"Fixo"}
+];
+
+app.listen(process.env.PORT || 3412);
+
+app.all('*',function(req,res,next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.get("/contacts",function(reqmres){
+    res.json(contacts);
+});
+
+
+app.get("/operators",function(req,res){
+  res.json(operators);
+});
